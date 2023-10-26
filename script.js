db89484750d149f4090f14be50686ff4608aeb9d
sandeep-chakraborty <anjanchakroborty17@gmail.com>
2023-10-26 12:30:15 +0530

    Adding files

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateTime() {
+  const cur = new Date();
+  const ye = cur.getFullYear();
+  const day = cur.toLocaleDateString("en-US", { weekday: "long" }) + ",";
+  const date = cur.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+  })+"th";
+  const hours = cur.getHours().toString().padStart(2, "0")+" :";
+  const minutes = cur.getMinutes().toString().padStart(2, "0")+" :";
+  const seconds = cur.getSeconds().toString().padStart(2, "0");
+  document.getElementById("day").textContent = day;
+  document.getElementById("date").textContent = date;
+  document.getElementById("year").textContent = ye;
+  document.getElementById("hour").textContent = hours;
+  document.getElementById("min").textContent = minutes;
+  document.getElementById("sec").textContent = seconds;
+}
+
+setInterval(updateTime, 1000);
