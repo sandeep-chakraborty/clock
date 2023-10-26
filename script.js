fbbd70eb5358898f7f6eddf1a647207e00d7cf62
sandeep-chakraborty <anjanchakroborty17@gmail.com>
2023-10-26 12:47:06 +0530

    Merge changes from remote repository

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function updateTime() {
   })+"th";
   const hours = cur.getHours().toString().padStart(2, "0")+" :";
   const minutes = cur.getMinutes().toString().padStart(2, "0")+" :";
+  const hour = cur.getHours().toString().padStart(2, "0")+":";
+  const minute = cur.getMinutes().toString().padStart(2, "0");
   const seconds = cur.getSeconds().toString().padStart(2, "0");
   document.getElementById("day").textContent = day;
   document.getElementById("date").textContent = date;
@@ -15,6 +17,7 @@ function updateTime() {
   document.getElementById("hour").textContent = hours;
   document.getElementById("min").textContent = minutes;
   document.getElementById("sec").textContent = seconds;
+  document.querySelector('title').textContent = `${hour}${minute}`;
 }
 
 setInterval(updateTime, 1000);
